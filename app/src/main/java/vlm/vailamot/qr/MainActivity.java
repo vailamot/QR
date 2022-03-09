a69f80281b9ad3044501392afea3933c1adf078b
vailamot <doanhtqvlm@gmail.com>
2022-03-10 00:22:28 +0700

    add qr generator

--- app/src/main/java/vlm/vailamot/qr/MainActivity.java
+++ app/src/main/java/vlm/vailamot/qr/MainActivity.java
@@ -3,7 +3,13 @@ package vlm.vailamot.qr;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,8 +23,13 @@ import com.journeyapps.barcodescanner.BarcodeEncoder;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
-    Button btn,btn2;
+    Button btn,btn2,btnGet;
     TextView tv;
     ImageView iv;
     EditText et;
@@ -33,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         tv = (TextView)  findViewById(R.id.tv);
         iv = (ImageView) findViewById(R.id.iv);
         et = (EditText) findViewById(R.id.et);
+        btnGet = (Button) findViewById(R.id.btnGet);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,6 +67,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btnGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //openChrome(view);
+                String s = tv.getText().toString();
+                open(s);
+            }
+        });
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clipData = ClipData.newPlainText("value",tv.getText().toString());
+                clipboardManager.setPrimaryClip(clipData);
+                Toast.makeText(MainActivity.this, "Copied", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
     // Register the launcher and result handler
@@ -73,6 +104,28 @@ public class MainActivity extends AppCompatActivity {
         ScanOptions options = new ScanOptions();
         options.setPrompt("Scan a QR code");
         barcodeLauncher.launch(options);
+    }
+
+    public boolean check(String s) {
+        try {
+            URI u = new URL(s).toURI();
+            return true;
+        } catch (Exception e){
+            return  false;
+        }
+    }
+
+    public void open(String s) {
+        if (check(s)) {
+            Uri uri = Uri.parse(s);
+            startActivity(new Intent(Intent.ACTION_VIEW,uri));
+        }else {
+            Intent intent = getPackageManager().getLaunchIntentForPackage("com.android.chrome");
+            startActivity(intent);
+        }
+
+
+
     }
 
 
